8c795a518ef0b0a65cc88a21d74989712abb1b38
滝口結 <[525tkg@gmail.com]>
2020-10-15 02:41:48 +0900

    Add Cancel Function

--- app/urls.py
+++ app/urls.py
@@ -8,7 +8,6 @@ from .views import *
 
 
 urlpatterns = [
-    # path('',  index_img, name='index_img'),
     path('',  InfoFilterView.as_view(), name=''),
 
     path('info_create/', InfoCreateView.as_view(), name='info_create'),
@@ -30,4 +29,5 @@ urlpatterns = [
     path('profile/', UserProfileView.as_view(), name='profile'),
     path('bought/<int:pk>/', UserBoughtView.as_view(), name='bought'),
 
+    path('bought_cancel/', UserBoughtCancelView.as_view(), name='bought_cancel'),
 ]
--- app/views.py
+++ app/views.py
@@ -18,17 +18,29 @@ from .forms import *
 User = get_user_model()
 
 
-class UserSignUpView(CreateView):
+
+class UserBoughtCancelView(LoginRequiredMixin, DeleteView):
     model = User
-    form_class = UserCreateForm
-    success_url = reverse_lazy('')
-    template_name = 'templates/app/user_signup.html'
+    template = loader.get_template('templates/app/user_bought.html')
 
-
-class UserProfileView(LoginRequiredMixin, View):
 
     def get(self, *args, **kwargs):
-        return render(self.request,'templates/app/user_profile.html')
+
+        users = User.objects.get(id=self.request.user.id) #a@gmail.com
+        user_buys_queryset = users.buys.all() #<QuerySet [<Item: 1>, <Item: 2>]>
+        user_buys=list(user_buys_queryset.values()) #{'id': 1, 'name': '1', 'age': 1, 'sex': 1, 'memo': '1', 'created_at': datetime.datetime(2020, 10, 12, 10, 16, 13, 443189, tzinfo=<UTC>), 'description': '1', 'photo': 'documents/pexels-melvin-buezo-2529148_lsypz0n.jpg', 'uploaded_at': datetime.datetime(2020, 10, 12, 10, 16, 13, 444163, tzinfo=<UTC>)}, {'id': 2, 'name': '2', 'age': 2, 'sex': 1, 'memo': '2', 'created_at': datetime.datetime(2020, 10, 12, 10, 37, 48, 983327, tzinfo=<UTC>), 'description': '2', 'photo': 'documents/pexels-la-miko-3616764_0qKhRNP.jpg', 'uploaded_at': datetime.datetime(2020, 10, 12, 10, 37, 48, 984092, tzinfo=<UTC>)}]
+
+        if self.request.GET.get('title', None):
+            str_value = self.request.GET.get('title', None) #2
+            int_value = int(str_value)-1 #1
+
+        if self.request.GET.getlist('title', None):
+            str_value=self.request.GET.getlist('title', None)[0]
+
+        users.buys.remove(user_buys_queryset[int_value])
+
+        return redirect('index')
+
 
 
 class UserBoughtView(LoginRequiredMixin, FilterView):
@@ -52,6 +64,20 @@ class UserBoughtView(LoginRequiredMixin, FilterView):
         }
 
         return HttpResponse(template.render(context))
+
+
+
+class UserSignUpView(CreateView):
+    model = User
+    form_class = UserCreateForm
+    success_url = reverse_lazy('')
+    template_name = 'templates/app/user_signup.html'
+
+
+class UserProfileView(LoginRequiredMixin, View):
+
+    def get(self, *args, **kwargs):
+        return render(self.request,'templates/app/user_profile.html')
 
 
 
@@ -102,11 +128,6 @@ class ItemFilterView(FilterView):
         return super().get(request, **kwargs)
 
 
-
-
-
-
-
     #いいねボタンみたいなbuy
     def Buys(request):
         item = get_object_or_404(Item, id=request.POST.get('item_id'))
@@ -125,9 +146,6 @@ class ItemFilterView(FilterView):
         if request.is_ajax():
             html = render_to_string('templates/app/item_filter.html', context, request=request )
             return JsonResponse({'form': html})
-
-
-
 
 
 
