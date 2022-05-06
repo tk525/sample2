85af3262ede27b21b6f01f03a66663e21aba342f
滝口結 <[525tkg@gmail.com]>
2022-05-07 02:27:23 +0900

    ポートフォリオのため

--- project/local_setting.py
+++ project/local_setting.py
@@ -3,7 +3,7 @@ import os
 #settings.pyからそのままコピー
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'zlqr)*uwsv8zqfwnjk2d)isms(x=g4ha4-w5q%uc2=0r2whi(0'
 #settings.pyからそのままコピー
 DATABASES = {
     'default': {
